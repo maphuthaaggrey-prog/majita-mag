e1ccf55084a00cc6fc14e19d6b043d93b5933048
maphuthaaggrey-prog <maphuthaaggrey83@gmail.com>
2025-03-07 12:55:03 +0200

    url image

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Hero from '../../components/Hero';
 import LatestTopics from '../../components/LatestTopics';
+import majitamag from '../icons/majitamag,jpg'
 import MusicNews from "../../components/MusicNews";
 import EventsComponent from "../../components/EventsComponent";
 import { Helmet, HelmetProvider } from 'react-helmet-async';
@@ -10,6 +11,7 @@ const Home = () => {
       <Helmet>
         <title>Majita Mag</title>
         <meta property="og:description" content="The ultimate showcase for forward-thinking creatives! Each issue is packed with inspiring stories, stunning visuals, and expert insights from the most talented minds in the industry." />
+        <meta property="og:image" content={majitamag} />
       </Helmet>
       <div className="wrap">
         <div className="home-container">
