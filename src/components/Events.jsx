f14b77b63e0b28c42e94cc62d1fe6da1651941c1
maphuthaaggrey-prog <maphuthaaggrey83@gmail.com>
2025-03-10 14:42:36 +0200

    latest events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -71,7 +71,7 @@ const Events = () => {
                         </div>
 
                         <div>
-                       
+                                <LatestEvents />
                         </div>
                     </div>
                 </div>
