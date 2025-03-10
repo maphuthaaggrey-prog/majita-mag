7cceb00bdbd9e5593e966c9639f0db65d9f66f24
maphuthaaggrey-prog <maphuthaaggrey83@gmail.com>
2025-03-10 18:51:38 +0200

    notfound page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom'
 const NotFound = () => {
     return ( 
-        <div id="creative-details" style={{ marginTop: '2em' }}>
-            <div style={{ textAlign: 'center', padding: '50px' }}>
-            <h1>404 - Page Not Found</h1>
-            <p>The page you're looking for doesn't exist.</p>
-            <Link to="/">Go back to the homepage</Link>
+        <div id="creative-details" style={{height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ textAlign: 'center' }}>
+            <h1 style={{fontSize: '87px'}}>404</h1>
+            <h1 style={{fontSize: '30px'}}>Page Not Found</h1>
+            <p style={{fontSize: '15px', marginBottom: '2em'}}>The page you're looking for doesn't exist.</p>
+            <Link to="https://www.majitamag.co.za/"style={{fontWeight: '700', color: 'black', textDecoration: 'none', backgroundColor: 'white', padding: '1em 2em', borderRadius: '100px'}}>Go back to the homepage</Link>
             </div>
       </div>
 
